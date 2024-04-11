4c9d0299c93f5b71ffaa9164b4c2191d7f9803ab
LRW-Calen <135932489+LRW-Calen@users.noreply.github.com>
2024-04-11 13:42:20 +0800

    src/test

---
+++ src/test/java/buildcraft/test/VanillaSetupBaseTester.java
@@ -0,0 +1,22 @@
+package buildcraft.test;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import net.minecraft.server.Bootstrap;
+import org.junit.BeforeClass;
+
+public class VanillaSetupBaseTester {
+    @BeforeClass
+    public static void init() {
+        System.out.println("INIT");
+        PrintStream sysOut = System.out;
+        InputStream sysIn = System.in;
+
+//        Bootstrap.register();
+        Bootstrap.bootStrap();
+
+        System.setIn(sysIn);
+        System.setOut(sysOut);
+    }
+}
---
+++ src/test/java/buildcraft/test/lib/misc/MessageUtilTester.java
@@ -0,0 +1,46 @@
+package buildcraft.test.lib.misc;
+
+import io.netty.buffer.Unpooled;
+
+import net.minecraft.network.FriendlyByteBuf;
+import org.junit.Assert;
+import org.junit.Test;
+
+import buildcraft.lib.misc.MessageUtil;
+
+public class MessageUtilTester {
+    @Test
+    public void testBooleanArraySmall() {
+        boolean[] expected = { false, true, false };
+
+//        PacketBuffer buffer = new PacketBuffer(Unpooled.buffer());
+        FriendlyByteBuf buffer = new FriendlyByteBuf(Unpooled.buffer());
+        MessageUtil.writeBooleanArray(buffer, expected);
+        boolean[] got = MessageUtil.readBooleanArray(buffer, expected.length);
+        Assert.assertArrayEquals(expected, got);
+    }
+
+    @Test
+    public void testBooleanArrayLarge() {
+        boolean[] expected = { false, true, false, false, false, true, true, true, true, true, true, true, false };
+
+//        PacketBuffer buffer = new PacketBuffer(Unpooled.buffer());
+        FriendlyByteBuf buffer = new FriendlyByteBuf(Unpooled.buffer());
+        MessageUtil.writeBooleanArray(buffer, expected);
+        boolean[] got = MessageUtil.readBooleanArray(buffer, expected.length);
+        Assert.assertArrayEquals(expected, got);
+    }
+
+    @Test
+    public void testBooleanArrayLargeManual() {
+        boolean[] expected = { false, true, false, false, false, true, true, true, true, true, true, true, false };
+
+//        PacketBuffer buffer = new PacketBuffer(Unpooled.buffer());
+        FriendlyByteBuf buffer = new FriendlyByteBuf(Unpooled.buffer());
+        MessageUtil.writeBooleanArray(buffer, expected);
+
+        boolean[] got = new boolean[expected.length];
+        MessageUtil.readBooleanArray(buffer, got);
+        Assert.assertArrayEquals(expected, got);
+    }
+}
